1cd93c4dc7184dc65e5962fe97a801fdcd32e479
Takashi_A <worldlead.aoyama1030@gmail.com>
2022-03-31 10:54:26 +0900

    [fix] ファイル名を変更

--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import { I18n } from "aws-amplify";
-import dictionary from "./library/dictionary";
+import dictionary from "./lib/dictionary";
 
 // 日本語化
 I18n.putVocabularies(dictionary);
